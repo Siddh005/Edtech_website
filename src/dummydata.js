a4726546e1b7b7d5adfff10062fac9415b0d4448
Siddh005 <Siddharthraj502@gmail.com>
2023-07-28 02:30:55 +0530

    gd

--- src/dummydata.js
+++ src/dummydata.js
@@ -44,7 +44,7 @@ export const awrapper = [
 export const coursesCard = [
   {
     id: 1,
-    cover: "../images/courses/c1.png",
+    cover: "./images/courses/c1.png",
     coursesName: "Introducing to Software Engineering",
     courTeacher: [
       {
@@ -58,7 +58,7 @@ export const coursesCard = [
   },
   {
     id: 2,
-    cover: "../images/courses/c2.png",
+    cover: "./images/courses/c2.png",
     coursesName: "Enhancing Adobe Photoshop CC 2020 Skills",
     courTeacher: [
       {
@@ -72,7 +72,7 @@ export const coursesCard = [
   },
   {
     id: 3,
-    cover: "../images/courses/c3.png",
+    cover: "./images/courses/c3.png",
     coursesName: "HTML, CSS, and Javascript for Web Developers",
     courTeacher: [
       {
@@ -86,7 +86,7 @@ export const coursesCard = [
   },
   {
     id: 4,
-    cover: "../images/courses/c4.png",
+    cover: "./images/courses/c4.png",
     coursesName: "Introducing to Programming with WordPress",
     courTeacher: [
       {
@@ -100,7 +100,7 @@ export const coursesCard = [
   },
   {
     id: 5,
-    cover: "../images/courses/c5.png",
+    cover: "./images/courses/c5.png",
     coursesName: "Introducing to Programming with ReactJS",
     courTeacher: [
       {
@@ -114,7 +114,7 @@ export const coursesCard = [
   },
   {
     id: 6,
-    cover: "../images/courses/c6.png",
+    cover: "./images/courses/c6.png",
     coursesName: "Learn Frontend Programming Language",
     courTeacher: [
       {
@@ -128,7 +128,7 @@ export const coursesCard = [
   },
   {
     id: 7,
-    cover: "../images/courses/c7.png",
+    cover: "./images/courses/c7.png",
     coursesName: "Introducing to with HTML / CSS",
     courTeacher: [
       {
@@ -142,7 +142,7 @@ export const coursesCard = [
   },
   {
     id: 8,
-    cover: "../images/courses/c8.png",
+    cover: "./images/courses/c8.png",
     coursesName: "Introducing to with JAVA",
     courTeacher: [
       {
@@ -156,7 +156,7 @@ export const coursesCard = [
   },
   {
     id: 9,
-    cover: "../images/courses/c9.png",
+    cover: "./images/courses/c9.png",
     coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
     courTeacher: [
       {
@@ -341,7 +341,7 @@ export const blog = [
     com: "3 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "The great aim of education is not knowledge, but action.",
-    cover: "../images/blog/b1.webp",
+    cover: "./images/blog/b1.webp",
   },
   {
     id: 2,
@@ -350,7 +350,7 @@ export const blog = [
     com: "5 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "The great aim of education is not knowledge, but action.",
-    cover: "../images/blog/b2.webp",
+    cover: "./images/blog/b2.webp",
   },
   {
     id: 3,
@@ -359,7 +359,7 @@ export const blog = [
     com: "10 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "The great aim of education is not knowledge, but action.",
-    cover: "../images/blog/b3.webp",
+    cover: "./images/blog/b3.webp",
   },
   {
     id: 4,
@@ -368,7 +368,7 @@ export const blog = [
     com: "3 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "The great aim of education is not knowledge, but action.",
-    cover: "../images/blog/b4.webp",
+    cover: "./images/blog/b4.webp",
   },
   {
     id: 5,
@@ -377,7 +377,7 @@ export const blog = [
     com: "3 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "The great aim of education is not knowledge, but action.",
-    cover: "../images/blog/b5.webp",
+    cover: "./images/blog/b5.webp",
   },
   {
     id: 6,
@@ -386,7 +386,7 @@ export const blog = [
     com: "12 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "The great aim of education is not knowledge, but action.",
-    cover: "../images/blog/b6.webp",
+    cover: "./images/blog/b6.webp",
   },
 ]
 export const testimonal = [
